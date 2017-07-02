6ba4a0c93bb8d689ba49a77464bf14ca4b005834
Valentine Svensson <valentine@nxn.se>
2017-07-02 23:32:33 +0100

    Structured nb-pca script'

--- mini-batch-sgd/zeisel_nb_pca.py
+++ mini-batch-sgd/zeisel_nb_pca.py
@@ -1,116 +1,121 @@
+import click
 import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-## Data loading ##
+tf.logging.set_verbosity(tf.logging.INFO)
 
-data = pd.read_csv('grun_sample_long_counts.csv', index_col=0)
-
-print(data.head())
-
-G = data['gene'].unique().shape[0]
-S = data['cell'].unique().shape[0]
-
-print('Shuffling...')
-data = data.sample(frac=1)
-NN = data.shape[0]
-
-def get_batch(data, batch_size, i):
+def next_batch(data, batch_size, i, NN):
     indx = (batch_size * i) % NN
     if (batch_size + indx) > NN:
         indx = 1
 
     return data.iloc[indx:indx + batch_size]
 
-## CONFIG ##
-
-N = 2  # Latent space dimensionality
-
-batch_size = 10000
-num_iter = 1000
-learning_rate = 1 / np.sqrt(batch_size)
-
-inner_iter = 3
-report_every = 200
 
 ## Model ##
+@click.command()
+@click.argument('input_file')
+@click.option('--obs_col', default='cell')
+@click.option('--var_col', default='gene')
+@click.option('--val_col', default='expression')
+@click.option('--offset_col', default='total_count')
+@click.option('--batch_size', default=10000)
+@click.option('--num_iter', default=2000)
+@click.option('--learning_rate', default=0.01)
+@click.option('--inner_iter', default=5)
+@click.option('--report_every', default=100)
+def main(input_file, obs_col, var_col, val_col, offset_col, batch_size,
+         num_iter, learning_rate, inner_iter, report_every):
 
-W = tf.Variable(np.random.randn(G, N), name='weights')
-x = tf.Variable(np.random.randn(N, S), name='PCs')
-E = tf.Variable(np.random.randn(S), name='Efficiency')
-S = tf.Variable(np.array([0.]), name='Scaling')
+    ## Data loading ##
 
-sample_idx = tf.placeholder(tf.int32, shape=[None])
-variable_idx = tf.placeholder(tf.int32, shape=[None])
-T_ = tf.placeholder(tf.float64, shape=[None])
-y_ = tf.placeholder(tf.float64, shape=[None])
+    data = pd.read_csv(input_file)
+
+    G = data[var_col].unique().shape[0]
+    S = data[obs_col].unique().shape[0]
+
+    tf.logging.info('Shuffling...')
+    data = data.sample(frac=1)
+    NN = data.shape[0]
+
+    ## CONFIG ##
+
+    N = 2  # Latent space dimensionality
+
+    ## Model ##
+
+    W = tf.Variable(np.random.randn(G, N), name='weights')
+    x = tf.Variable(np.random.randn(N, S), name='PCs')
+    E = tf.Variable(np.random.randn(S), name='Efficiency')
+    S = tf.Variable(np.array([0.]), name='Scaling')
+
+    sample_idx = tf.placeholder(tf.int32, shape=[None])
+    variable_idx = tf.placeholder(tf.int32, shape=[None])
+    T_ = tf.placeholder(tf.float64, shape=[None])
+    y_ = tf.placeholder(tf.float64, shape=[None])
 
 
-W_ = tf.gather(W, variable_idx)
-x_ = tf.gather(tf.matrix_transpose(x), sample_idx)
-eta_ = tf.reduce_sum(W_ * x_, 1)
-E_ = tf.gather(E, sample_idx)
+    W_ = tf.gather(W, variable_idx)
+    x_ = tf.gather(tf.matrix_transpose(x), sample_idx)
+    eta_ = tf.reduce_sum(W_ * x_, 1)
+    E_ = tf.gather(E, sample_idx)
 
-mu_ = tf.exp(eta_ + tf.log(T_) + E_ + S)
+    mu_ = tf.exp(eta_ + tf.log(T_) + E_ + S)
 
-LL = tf.reduce_sum(y_ * tf.log(mu_) - (y_ + 1) * tf.log(mu_ + 1))
+    LL = tf.reduce_sum(y_ * tf.log(mu_) - (y_ + 1) * tf.log(mu_ + 1))
 
-cost = -LL / batch_size
+    cost = -LL / batch_size
 
-optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
+    optimizer = tf.train.AdamOptimizer(learning_rate).minimize(cost)
 
-init = tf.global_variables_initializer()
+    init = tf.global_variables_initializer()
 
-costs = np.zeros(num_iter)
+    costs = np.zeros(num_iter)
 
-config = tf.ConfigProto(device_count={'CPU': 4},
-                        inter_op_parallelism_threads=4,
-                        intra_op_parallelism_threads=1)
-config.graph_options.optimizer_options.opt_level = -1
+    tf.logging.info('Training')
+    with tf.Session() as sess:
+        sess.run(init)
 
-print('Training')
-with tf.Session(config=config) as sess:
-    sess.run(init)
+        for i in range(num_iter):
+            batch = next_batch(data, batch_size, i, NN)
+            feed_dict = {sample_idx: batch[obs_col],
+                        variable_idx: batch[var_col],
+                        y_: batch[val_col],
+                        T_: batch[offset_col]}
 
-    for i in range(num_iter):
-        batch = get_batch(data, batch_size, i)
-        feed_dict = {sample_idx: batch['cell'],
-                     variable_idx: batch['gene'],
-                     y_: batch['expression'],
-                     T_: batch['total_count']}
+            for j in range(inner_iter):
+                sess.run(optimizer, feed_dict=feed_dict)
 
-        for j in range(inner_iter):
-            sess.run(optimizer, feed_dict=feed_dict)
+            c = sess.run(cost, feed_dict=feed_dict)
+            costs[i] = c
 
-        c = sess.run(cost, feed_dict=feed_dict)
-        costs[i] = c
+            if not i % report_every:
+                tf.logging.info('Cost: {}'.format(c))
+        
+        X_result = sess.run(x)
+        S_result = sess.run(S)
+        E_result = sess.run(E)
 
-        if not i % report_every:
-            print('Cost: {}'.format(c))
-    
-    X_result = sess.run(x)
-    S_result = sess.run(S)
-    E_result = sess.run(E)
+    import matplotlib.pyplot as plt
 
-print(X_result)
-print(S_result)
+    plt.rcParams['axes.spines.right'] = False
+    plt.rcParams['axes.spines.top'] = False
 
-import matplotlib.pyplot as plt
+    plt.figure(figsize=(9, 4))
+    plt.subplot(1, 2, 1)
+    plt.scatter(X_result[0], X_result[1], s=10, alpha=0.33, c=E_result)
+    plt.colorbar()
+    plt.xlabel('x1')
+    plt.ylabel('x2')
 
-plt.rcParams['axes.spines.right'] = False
-plt.rcParams['axes.spines.top'] = False
+    plt.subplot(1, 2, 2)
+    plt.plot(costs, c='k')
+    plt.xlabel('Iteration')
+    plt.ylabel('Cost')
 
-plt.figure(figsize=(9, 4))
-plt.subplot(1, 2, 1)
-plt.scatter(X_result[0], X_result[1], s=10, alpha=0.33, c=E_result)
-plt.colorbar()
-plt.xlabel('z1')
-plt.ylabel('z2')
+    plt.tight_layout()
+    plt.show()
 
-plt.subplot(1, 2, 2)
-plt.plot(costs, c='k')
-plt.xlabel('Iteration')
-plt.ylabel('Cost')
-
-plt.tight_layout()
-plt.show()
+if __name__ == '__main__':
+    main()
